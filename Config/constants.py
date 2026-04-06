ff9d48b87fd4e464e0995e1fcde3e10f3b668a9c
danilo <danilo.mandusic1@gmail.com>
2026-04-06 17:44:17 +0200

    added data preprocessing

--- Config/constants.py
+++ Config/constants.py
@@ -8,9 +8,14 @@ import sys
 ROOT_DIR = Path(__file__).resolve().parents[1]
 sys.path.append(str(ROOT_DIR))
 RAW_DATA_DIR = ROOT_DIR / "Data/Raw"
+PROCESSED_DATA_DIR = ROOT_DIR / "Data/Processed"
 
 # eda
 RANDOM_SAMPLE_SIZE = 20 # random sample grid visualization size
 DELIMITER = ";"
 IMAGE_FORMAT = "ppm"
 TABLE_FORMAT = "csv"
+
+# preprocessing
+TRACK_LENGTH = 30
+SAMPLES_LIMIT = 300
